a4378feb86086bc7324324f59164084222de8084
adjunto1 <adjunto183@gmail.com>
2026-05-18 11:14:54 -0300

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAuva1NIWiRO29jluYKrgjuUnGSgbAgQbs",
+  authDomain: "programa-icv.firebaseapp.com",
+  databaseURL: "https://programa-icv-default-rtdb.firebaseio.com",
+  projectId: "programa-icv",
+  storageBucket: "programa-icv.firebasestorage.app",
+  messagingSenderId: "532656820853",
+  appId: "1:532656820853:web:f703c9c61ccbc438619e3c"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: '/logo192.png',
+    badge: '/logo192.png',
+  });
+});
